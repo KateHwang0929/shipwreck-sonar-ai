6b6b5c65a1d928a96e17390b57f83d826b498e98
Kate Hwang <kateh6@illinois.edu>
2026-07-10 13:44:09 +0900

    Fix dataset package imports

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,6 +1,5 @@
 """Dataset loaders for sonar segmentation."""
 
-from .toy_dataset import ToySonarDataset
 from .ai4shipwrecks_dataset import AI4ShipwrecksDataset
 
-__all__ = ["ToySonarDataset", "AI4ShipwrecksDataset"]
+__all__ = ["AI4ShipwrecksDataset"]
